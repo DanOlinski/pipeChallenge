e6140dbf055846d96abd096570c0a4638e904f39
DanOlinski <daniel.guterres@hotmail.com>
2024-07-04 13:48:20 -0600

    t

--- sinkSystem.js
+++ sinkSystem.js
@@ -480,12 +480,3 @@ const sinkSystem = (filePath) => {
   }
 };
 
-console.log(sinkSystem('./testData/dataABG.txt'));
-console.log(sinkSystem('./testData/dataABGPR.txt'));
-console.log(sinkSystem('./testData/dataABZ.txt'));
-console.log(sinkSystem('./testData/dataAC.txt'));
-console.log(sinkSystem('./testData/dataAG.txt'));
-console.log(sinkSystem('./testData/dataAGM.txt'));
-console.log(sinkSystem('./testData/dataB.txt'));
-console.log(sinkSystem('./testData/dataNoSorce.txt'));
-console.log(sinkSystem('./testData/dataNotConnected.txt'));
